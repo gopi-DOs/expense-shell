65fb05db79ba8074f45e7c7fb10bba63b8ceceff
gopi-DOs <gopiseelam93@gmail.com>
2024-11-14 13:29:01 +0530

    mysql

---
+++ mysql.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+CHECK_ROOT(){
+   if [ $USERID -ne 0 ]
+   then
+        echo -e "$R please run the script with root privrlages $N"  | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+      echo -e "$2 is...$R  failed $N"  | tee -a $LOG_FILE
+      exit 1
+    else 
+      echo -e "$2...$G  success $N"  | tee -a $LOG_FILE
+      fi 
+}
+
+echo "script started executing at: $(date)" | tee -a $LOG_FILE
+
+CHECK_ROOT 
+ 
+  dnf install mysql-server -y
+  VALIDATE $? "Installing Mysql server"
+
+  systemctl enable mysqld
+  VALIDATE $? "Enabling mysql server"
+
+  systemctl start mysqld
+  VALIDATE $? "started mysql server"
