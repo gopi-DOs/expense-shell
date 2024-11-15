2e9b0d36b100813e26f4a89667af5160ba8075ff
gopi-DOs <gopiseelam93@gmail.com>
2024-11-15 15:00:51 +0530

    mysql

--- backend.sh
+++ backend.sh
@@ -72,7 +72,7 @@ cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.serv
 dnf install mysql -y &>>$LOG_FILE
 VALIDATE $? "Installing mysql client"
 
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h mysql.bng81s.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
 VALIDATE $? "schema loading"
 
 systemctl daemon-reload  &>>$LOG_FILE
