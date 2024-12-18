aeb5c36c787f84b9da8e6c74947cf3485652cc02
gopi-DOs <gopiseelam93@gmail.com>
2024-12-18 21:14:42 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -46,8 +46,8 @@ VALIDATE $? "start mysql"
 mysql -h mysql.bng81s.online -u root -pExpense@1  &>>$LOG_FILE
 if [ $? -ne 0 ]
 then 
-   echo "mysql root password is not setup setting now"
-   mysql_secure_installation --set-root-pass ExpenseApp@1    &>>$LOG_FILE
+   echo "mysql root password is not setup setting now"    &>>$LOG_FILE
+   mysql_secure_installation --set-root-pass ExpenseApp@1    
    VALIDATE $? "setting up root password"
 else 
    echo -e "mysql root password is already setup  $Y  skipping  $N" | tee -a $LOG_FILE
